d134ef1c1a8fa6ff95a3297a8fc9cdeb3443b0a1
Ivor Wanders <ivor@iwanders.net>
2024-08-30 23:20:59 +0000

    Still nothing, lets start with dcomp, work to transparency.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,18 @@ use windows::{
     core::*, Win32::Foundation::*, Win32::Graphics::Gdi::ValidateRect,
     Win32::System::LibraryLoader::GetModuleHandleA, Win32::UI::WindowsAndMessaging::*,
 };
-
-use windows::Win32::Graphics::GdiPlus::{GdipCreateFromHWND, GpGraphics, GpPen};
+use windows::{
+    core::*,
+    Foundation::Numerics::*,
+    Win32::{
+        Foundation::*, Graphics::Direct2D::Common::*, Graphics::Direct2D::*, Graphics::Direct3D::*,
+        Graphics::Direct3D11::*, Graphics::DirectComposition::*, Graphics::DirectWrite::*,
+        Graphics::Dxgi::Common::*, Graphics::Dxgi::*, Graphics::Gdi::*, Graphics::Imaging::D2D::*,
+        Graphics::Imaging::*, System::Com::*, System::LibraryLoader::*, UI::Animation::*,
+        UI::HiDpi::*, UI::Shell::*, UI::WindowsAndMessaging::*,
+    },
+};
+// use windows::Win32::Graphics::GdiPlus::{GdipCreateFromHWND, GpGraphics, GpPen};
 
 /*
 How do we make transparent pixels in our new overlay? Just specifying transparency does not work.
@@ -75,6 +85,120 @@ fn rgb(r: u8, g: u8, b: u8) -> windows::Win32::Foundation::COLORREF {
 fn rgba(r: u8, g: u8, b: u8, a: u8) -> windows::Win32::Foundation::COLORREF {
     windows::Win32::Foundation::COLORREF((a as u32) << 24 | (r as u32) << 16 | (g as u32) << 8 | b as u32)
 }
+
+fn create_device_3d() -> Result<ID3D11Device> {
+    let mut device = None;
+
+    unsafe {
+        D3D11CreateDevice(
+            None,
+            D3D_DRIVER_TYPE_HARDWARE,
+            None,
+            D3D11_CREATE_DEVICE_BGRA_SUPPORT,
+            None,
+            D3D11_SDK_VERSION,
+            Some(&mut device),
+            None,
+            None,
+        )
+        .map(|()| device.unwrap())
+    }
+}
+
+fn create_device_2d(device_3d: &ID3D11Device) -> Result<ID2D1Device> {
+    let dxgi: IDXGIDevice3 = device_3d.cast()?;
+    unsafe { D2D1CreateDevice(&dxgi, None) }
+}
+
+
+fn create_visual(device: &IDCompositionDesktopDevice) -> Result<IDCompositionVisual2> {
+    unsafe {
+        let visual = device.CreateVisual()?;
+        visual.SetBackFaceVisibility(DCOMPOSITION_BACKFACE_VISIBILITY_HIDDEN)?;
+        Ok(visual)
+    }
+}
+
+fn create_surface(
+    device: &IDCompositionDesktopDevice,
+    width: f32,
+    height: f32,
+) -> Result<IDCompositionSurface> {
+    unsafe {
+        device.CreateSurface(
+            width as u32,
+            height as u32,
+            DXGI_FORMAT_B8G8R8A8_UNORM,
+            DXGI_ALPHA_MODE_PREMULTIPLIED,
+        )
+    }
+}
+
+
+fn create_text_format() -> Result<IDWriteTextFormat> {
+    unsafe {
+        let factory: IDWriteFactory2 = DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED)?;
+
+        let format = factory.CreateTextFormat(
+            w!("Candara"),
+            None,
+            DWRITE_FONT_WEIGHT_NORMAL,
+            DWRITE_FONT_STYLE_NORMAL,
+            DWRITE_FONT_STRETCH_NORMAL,
+            100.0 / 2.0,
+            w!("en"),
+        )?;
+
+        format.SetTextAlignment(DWRITE_TEXT_ALIGNMENT_CENTER)?;
+        format.SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER)?;
+        Ok(format)
+    }
+}
+
+
+fn draw_card_front(
+    surface: &IDCompositionSurface,
+    value: u8,
+    format: &IDWriteTextFormat,
+    brush: &ID2D1SolidColorBrush,
+) -> Result<()> {
+    unsafe {
+        let mut offset = Default::default();
+        let dc: ID2D1DeviceContext = surface.BeginDraw(None, &mut offset)?;
+        dc.SetDpi(90.0, 90.0);
+
+        dc.SetTransform(&Matrix3x2::translation(
+            0.0,
+            0.0,
+            // physical_to_logical(offset.x as f32, dpi.0),
+            // physical_to_logical(offset.y as f32, dpi.1),
+        ));
+
+        dc.Clear(Some(&D2D1_COLOR_F {
+            r: 1.0,
+            g: 0.0,
+            b: 0.0,
+            a: 0.5,
+        }));
+
+        dc.DrawText(
+            &[value as _],
+            format,
+            &D2D_RECT_F {
+                left: 0.0,
+                top: 0.0,
+                right: 100.0,
+                bottom: 100.0,
+            },
+            brush,
+            D2D1_DRAW_TEXT_OPTIONS_NONE,
+            DWRITE_MEASURING_MODE_NATURAL,
+        );
+
+        surface.EndDraw()
+    }
+}
+
 
 pub fn main() -> Result<()> {
     // return d3d12_sample::main();
@@ -155,30 +279,69 @@ pub fn main() -> Result<()> {
 
         // UpdateLayeredWindow must happen PRIOR to set layered window attributes; https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-updatelayeredwindow
         // see https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setlayeredwindowattributes#remarks
-	SetLayeredWindowAttributes(hwnd, windows::Win32::Foundation::COLORREF(0x00_FF_FF_FF), 255, LWA_COLORKEY);
+	// SetLayeredWindowAttributes(hwnd, windows::Win32::Foundation::COLORREF(0x00_FF_FF_FF), 255, LWA_COLORKEY);
 	// SetLayeredWindowAttributes(hwnd, windows::Win32::Foundation::COLORREF(0xFF_FF_FF_FF), 128, LWA_ALPHA);
 
+        use windows::Win32::UI::HiDpi::*;
+        use windows::Win32::Graphics::Gdi::*;
+        let monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+        let mut dpi = (0, 0);
+        GetDpiForMonitor(monitor, MDT_EFFECTIVE_DPI, &mut dpi.0, &mut dpi.1)?;
+        // self.dpi = (dpi.0 as f32, dpi.1 as f32);
 
-        setup_gdi()?;
-        // let mut graphics: GpGraphics = Default::default();
-        let mut graphics: *mut GpGraphics = std::ptr::null_mut();
-        let gdip = GdipCreateFromHWND(hwnd, &mut graphics);
+        // if cfg!(debug_assertions) {
+            // println!("initial dpi: {:?}", self.dpi);
+        // }
 
-        // let mut white_pen: *mut GpPen = std::ptr::null_mut();
-        // windows::Win32::Graphics::GdiPlus::GdipCreatePen1(0xFFFFFFFF, 3.0, windows::Win32::Graphics::GdiPlus::UnitPixel, &mut white_pen);
-        // let white_brush = windows::Win32::Graphics::Gdi::CreateSolidBrush(0xFFFFFFFF);
-        // windows::Win32::Graphics::GdiPlus::GdipFillRectangle(graphics, 
-        // windows::Win32::Graphics::Gdi::SetBkColor(windows::Win32::Graphics::Gdi::GetDC(hwnd), windows::Win32::Foundation::COLORREF(0xFF_FF_FF_FF));
-        // windows::Win32::Graphics::Gdi::SetBkMode(windows::Win32::Graphics::Gdi::GetDC(hwnd), windows::Win32::Graphics::Gdi::OPAQUE);
+        // let size = self.effective_window_size()?;
 
-        let mut white_pen: *mut GpPen = std::ptr::null_mut();
-        windows::Win32::Graphics::GdiPlus::GdipCreatePen1(0xFF_FF_FF_FF, 300000.0, windows::Win32::Graphics::GdiPlus::UnitPixel, &mut white_pen);
-        windows::Win32::Graphics::GdiPlus::GdipDrawLine(graphics, white_pen, 0.0, 0.0, 1920.0, 1080.0);
+        SetWindowPos(
+            hwnd,
+            None,
+            0,
+            0,
+            1920,
+            1080,
+            SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOZORDER,
+        );
 
-        let mut blue_pen: *mut GpPen = std::ptr::null_mut();
-        let color = rgba(0, 0, 0xff, 0x10);
-        windows::Win32::Graphics::GdiPlus::GdipCreatePen1(color.0, 3.0, windows::Win32::Graphics::GdiPlus::UnitPixel, &mut blue_pen);
-        windows::Win32::Graphics::GdiPlus::GdipDrawLine(graphics, blue_pen, 0.0, 0.0, 100.0, 100.0);
+        let device_3d = create_device_3d()?;
+        let device_2d = create_device_2d(&device_3d)?;
+        let device = Some(device_3d);
+        let desktop: IDCompositionDesktopDevice = DCompositionCreateDevice2(&device_2d)?;
+
+        // First release any previous target, otherwise `CreateTargetForHwnd` will find the HWND occupied.
+        // let mut target = None;
+        let target = desktop.CreateTargetForHwnd(hwnd, true)?;
+        let root_visual = create_visual(&desktop)?;
+        target.SetRoot(&root_visual)?;
+        // self.target = Some(target);
+
+        let dc = device_2d.CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE)?;
+
+        let brush = dc.CreateSolidColorBrush(
+            &D2D1_COLOR_F {
+                r: 1.0,
+                g: 0.0,
+                b: 0.0,
+                a: 0.5,
+            },
+            None,
+        )?;
+
+        let width = 10.0;
+        let height = 10.0;
+
+        let back_visual = create_visual(&desktop)?;
+        back_visual.SetOffsetX2(0.0)?;
+        back_visual.SetOffsetY2(0.0)?;
+        root_visual.AddVisual(&back_visual, false, None)?;
+
+        let format = create_text_format()?;
+
+        let front_surface = create_surface(&desktop, width, height)?;
+        back_visual.SetContent(&front_surface)?;
+        draw_card_front(&front_surface, 0x61, &format, &brush)?;
 
         let mut message = MSG::default();
 
