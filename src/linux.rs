e3de914350771bf1004f0a19ab86ef95d05af1c4
Ivor Wanders <ivor@iwanders.net>
2025-06-21 13:31:59 -0400

    Ensure null byte termination.

--- src/linux.rs
+++ src/linux.rs
@@ -196,13 +196,11 @@ impl OverlayImpl {
             let font_descriptor =
                 format!("{}:pixelsize={}", properties.font, properties.size as i32);
             let font_name = std::ffi::OsString::from(&font_descriptor);
-            let font_str = std::mem::transmute::<*const u8, _>(
-                font_name.as_os_str().as_encoded_bytes().as_ptr(),
-            );
+            let font_cstr = std::ffi::CString::new(font_name.as_os_str().as_encoded_bytes())?;
             let font = (xft.XftFontOpenName)(
                 self.display,
                 *self.screen.as_ref().ok_or("no screen")?,
-                font_str,
+                font_cstr.as_ptr(),
             );
             // println!("font prop: {font:?}");
             Ok(PreparedFont {
