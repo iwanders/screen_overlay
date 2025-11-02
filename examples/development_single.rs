a1340e9d850f4fcaa391b64f86d33d56522b3108
Ivor Wanders <ivor@iwanders.net>
2025-11-02 09:24:55 -0500

    It draws a triangle.

---
+++ examples/development_single.rs
@@ -0,0 +1,132 @@
+use screen_overlay::{
+    // CapStyle, CircleDirection, GeometryElement, LineJoin,
+    Color,
+    DashStyle,
+    DrawGeometry,
+    Error,
+    LineStyle,
+    Overlay,
+    OverlayConfig,
+    Point,
+    Rect,
+    Stroke,
+    TextAlignment,
+    TextProperties,
+};
+
+pub fn main() -> std::result::Result<(), Error> {
+    screen_overlay::setup()?;
+    let window = Overlay::new_with_config(&OverlayConfig {
+        name: "Awesome Overlay".to_owned(),
+        ..Default::default()
+    })?;
+
+    let twindow = window.clone();
+
+    let color = Color {
+        r: 255,
+        g: 0,
+        b: 255,
+        a: 128,
+    };
+    let alpha = 0.5;
+    println!("first image alloc");
+    let _image1 = twindow
+        .load_texture(std::path::PathBuf::from(
+            "PNG_transparency_demonstration_1.png",
+        ))
+        .expect("failed to load image");
+
+    println!("second image alloc");
+    let image2 = twindow
+        .load_texture(std::path::PathBuf::from(
+            "PNG_transparency_demonstration_1.png",
+        ))
+        .expect("failed to load image");
+    let _t2 = twindow
+        .draw_texture(
+            &Point::new(500.0, 500.0),
+            &image2,
+            &Rect::from(0.0, 0.0).sized(200.0, 200.0),
+            &color,
+            alpha,
+        )
+        .expect("texture draw failed");
+
+    /*
+        let i = 300;
+        let pos = Rect::from(200.0 + 50.0 * (i % 5) as f32, 200.0).sized(200.0, 300.0);
+        let geometry = DrawGeometry::new().rectangle(&pos);
+        let color = Color {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 255,
+        };
+        let stroke = Stroke { color, width: 1.0 };
+        let text_box_style = LineStyle {
+            dash_style: DashStyle::Dash,
+            // line_join: LineJoin::Round,
+            ..Default::default()
+        };
+
+        let _v = twindow
+            .draw_geometry(&geometry, &stroke, &text_box_style)
+            .expect("create image failed");
+
+        let font = twindow
+            .prepare_font(&TextProperties {
+                size: 32.0,
+                horizontal_align: TextAlignment::Min,
+                vertical_align: TextAlignment::Min,
+                ..Default::default()
+            })
+            .expect("preparing the font failed");
+
+        let color = Color {
+            r: 255,
+            g: 0,
+            b: 255,
+            a: 128,
+        };
+        let _v = twindow
+            .draw_text("hello there we are rendering text", &pos, &color, &font)
+            .expect("create image failed");
+
+        let geometry = DrawGeometry::new().circle(&Point::new(510.0, 500.0), 10.0);
+        let color = Color {
+            r: 0,
+            g: 255,
+            b: 255,
+            a: 255,
+        };
+        let stroke = Stroke { color, width: 1.0 };
+
+        let _v = twindow
+            .draw_geometry(&geometry, &stroke, &Default::default())
+            .expect("create image failed");
+
+        let _z = {
+            let geometry = DrawGeometry::new()
+                .hollow(200.0, 10.0)
+                .line(100.0, 100.0)
+                .closed();
+            let color = Color {
+                r: 255,
+                g: 0,
+                b: 255,
+                a: 255,
+            };
+            let stroke = Stroke { color, width: 30.0 };
+
+            let v = twindow
+                .draw_geometry(&geometry, &stroke, &Default::default())
+                .expect("create image failed");
+            std::thread::sleep(std::time::Duration::from_millis(500));
+            v
+        };
+    */
+    Ok(screen_overlay::block_and_loop()?)
+
+    // Ok(())
+}
--- src/linux.rs
+++ src/linux.rs
@@ -238,7 +238,7 @@ impl OverlayImpl {
             let glfw = &mut self.glfw;
             glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
             glfw.window_hint(glfw::WindowHint::OpenGlProfile(
-                glfw::OpenGlProfileHint::Core,
+                glfw::OpenGlProfileHint::Compat,
             ));
             glfw.window_hint(glfw::WindowHint::Decorated(false));
             glfw.window_hint(glfw::WindowHint::FocusOnShow(false));
@@ -557,7 +557,7 @@ impl OverlayImpl {
         let data = raw_container.as_ptr() as _;
         println!("going to load");
         unsafe {
-            self.window.as_mut().unwrap().make_current();
+            // self.window.as_mut().unwrap().make_current();
 
             let z = self.window_size.unwrap();
             // Make a texture.
@@ -565,7 +565,7 @@ impl OverlayImpl {
             let mut texture_target: u32 = 0;
             let tp = (&mut texture_target as *mut _) as *mut _;
             println!("tp: {:?}", tp);
-            gl::GenTextures(3000, tp);
+            gl::GenTextures(1, tp);
 
             println!("binding to load");
             // "Bind" the newly created texture : all future texture functions will modify this texture
@@ -668,8 +668,118 @@ impl OverlayImpl {
         color: &Color,
         alpha: f32,
     ) -> Result<IDVisual, Error> {
-        // let drawable = self.window.unwrap();
-        // let gc = self.gc.unwrap();
+        unsafe {
+            // https://github.com/rust-tutorials/learn-opengl/blob/ed6ae3250b06223882d99f993372efdbee4637e7/examples/001-triangle-arrays1.rs#L70C1-L169C1
+            type Vertex = [f32; 3];
+
+            const VERTICES: [Vertex; 3] = [
+                [-0.5, -0.5, 0.0], //
+                [0.5, -0.5, 0.0],
+                [0.0, 0.5, 0.0],
+            ];
+
+            const VERT_SHADER: &str = r#"#version 330 core
+              layout (location = 0) in vec3 pos;
+
+              void main() {
+                gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+              }
+            "#;
+
+            const FRAG_SHADER: &str = r#"#version 330 core
+              out vec4 final_color;
+
+              void main() {
+                final_color = vec4(1.0, 0.5, 0.2, 1.0);
+              }
+            "#;
+
+            let mut vao = 0;
+            gl::GenVertexArrays(1, &mut vao);
+            assert_ne!(vao, 0);
+            gl::BindVertexArray(vao);
+
+            let mut vbo = 0;
+            gl::GenBuffers(1, &mut vbo);
+            assert_ne!(vbo, 0);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                size_of_val(&VERTICES) as isize,
+                VERTICES.as_ptr().cast(),
+                gl::STATIC_DRAW,
+            );
+
+            gl::VertexAttribPointer(
+                0,
+                3,
+                gl::FLOAT,
+                gl::FALSE,
+                size_of::<Vertex>().try_into().unwrap(),
+                0 as *const _,
+            );
+            gl::EnableVertexAttribArray(0);
+
+            let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+            assert_ne!(vertex_shader, 0);
+            gl::ShaderSource(
+                vertex_shader,
+                1,
+                &(VERT_SHADER.as_bytes().as_ptr().cast()),
+                &(VERT_SHADER.len().try_into().unwrap()),
+            );
+            gl::CompileShader(vertex_shader);
+            let mut success = 0;
+            gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
+            if success == 0 {
+                let mut v: Vec<u8> = Vec::with_capacity(1024);
+                let mut log_len = 0_i32;
+                gl::GetShaderInfoLog(vertex_shader, 1024, &mut log_len, v.as_mut_ptr().cast());
+                v.set_len(log_len.try_into().unwrap());
+                panic!("Vertex Compile Error: {}", String::from_utf8_lossy(&v));
+            }
+
+            let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+            assert_ne!(fragment_shader, 0);
+            gl::ShaderSource(
+                fragment_shader,
+                1,
+                &(FRAG_SHADER.as_bytes().as_ptr().cast()),
+                &(FRAG_SHADER.len().try_into().unwrap()),
+            );
+            gl::CompileShader(fragment_shader);
+            let mut success = 0;
+            gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut success);
+            if success == 0 {
+                let mut v: Vec<u8> = Vec::with_capacity(1024);
+                let mut log_len = 0_i32;
+                gl::GetShaderInfoLog(fragment_shader, 1024, &mut log_len, v.as_mut_ptr().cast());
+                v.set_len(log_len.try_into().unwrap());
+                panic!("Fragment Compile Error: {}", String::from_utf8_lossy(&v));
+            }
+
+            let shader_program = gl::CreateProgram();
+            assert_ne!(shader_program, 0);
+            gl::AttachShader(shader_program, vertex_shader);
+            gl::AttachShader(shader_program, fragment_shader);
+            gl::LinkProgram(shader_program);
+            let mut success = 0;
+            gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success);
+            if success == 0 {
+                let mut v: Vec<u8> = Vec::with_capacity(1024);
+                let mut log_len = 0_i32;
+                gl::GetProgramInfoLog(shader_program, 1024, &mut log_len, v.as_mut_ptr().cast());
+                v.set_len(log_len.try_into().unwrap());
+                panic!("Program Link Error: {}", String::from_utf8_lossy(&v));
+            }
+            gl::DeleteShader(vertex_shader);
+            gl::DeleteShader(fragment_shader);
+
+            gl::UseProgram(shader_program);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            self.window.as_mut().unwrap().swap_buffers();
+        }
+
         return Ok(IDVisual::Image {});
         /*
         println!("gc: {:?}", gc);
