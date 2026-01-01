f422face666dbce686a8c5a3bc8b4c81170f9bf6
Ivor Wanders <ivor@iwanders.net>
2026-01-01 18:30:34 -0500

    Reshufle to explore winit as a backend.

---
+++ src/linux/with_winit.rs
@@ -0,0 +1,94 @@
+#![allow(unused_variables, unused_imports, unreachable_code)]
+use crate::{
+    CapStyle, CircleDirection, Color, DashStyle, DrawGeometry, Error, GeometryElement, LineJoin,
+    LineStyle, OverlayConfig, Point, Rect, Stroke, TextAlignment, TextProperties,
+};
+
+#[derive(Clone)]
+pub struct ImageTexture {}
+impl std::fmt::Debug for ImageTexture {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "ImageTexture ")
+    }
+}
+#[derive(Clone)]
+pub struct PreparedFont {}
+impl std::fmt::Debug for PreparedFont {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "PreparedFont  ")
+    }
+}
+// pub type IDVisual = usize;
+#[derive(Clone, Debug)]
+pub struct IDVisual {}
+
+pub struct OverlayImpl {}
+unsafe impl Send for OverlayImpl {}
+impl OverlayImpl {
+    pub fn new() -> Result<Self, crate::Error> {
+        Ok(Self {})
+    }
+
+    pub fn create_window(&mut self, config: &OverlayConfig) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub fn create_device_resources(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub fn draw_geometry(
+        &mut self,
+        geometry: &DrawGeometry,
+        stroke: &Stroke,
+        line_style: &LineStyle,
+    ) -> Result<IDVisual, Error> {
+        Ok(IDVisual {})
+    }
+
+    pub fn prepare_font(&mut self, properties: &TextProperties) -> Result<PreparedFont, Error> {
+        todo!()
+    }
+
+    pub fn draw_text(
+        &mut self,
+        text: &str,
+        layout: &Rect,
+        color: &Color,
+        font: &PreparedFont,
+    ) -> Result<IDVisual, Error> {
+        todo!();
+    }
+
+    pub fn load_texture<P: AsRef<std::path::Path>>(
+        &mut self,
+        path: P,
+    ) -> Result<ImageTexture, Error> {
+        Ok(ImageTexture {})
+    }
+
+    pub fn draw_texture(
+        &mut self,
+        position: &Point,
+        texture: &ImageTexture,
+        texture_region: &Rect,
+        color: &Color,
+        alpha: f32,
+    ) -> Result<IDVisual, Error> {
+        Ok(IDVisual {})
+    }
+
+    pub fn remove_visual(&mut self, visual: &IDVisual) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub fn render(&mut self) {}
+}
+
+pub fn run_msg_loop() -> Result<(), Error> {
+    Ok(())
+}
+
+pub fn setup() -> Result<(), Error> {
+    Ok(())
+}
